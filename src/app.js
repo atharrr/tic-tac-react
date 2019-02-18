15e9000b0f1aa3020e4d3dcfaa47a50b8f2be2e8
RealAther <Real.Ather@gmail.com>
2019-02-19 02:00:46 +0500

    :new:  Make 'Box' component and pass it onto 'MainApp' component

--- src/app.js
+++ src/app.js
@@ -1,56 +1,63 @@
 import React from "react";
 
+class Box extends React.Component {
+  render() {
+    const { handleClick } = this.props;
+    return <button onClick={handleClick} />;
+  }
+}
+
 class MainApp extends React.Component {
   render() {
     return (
       <div>
         <div>
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 1 has been clicked");
             }}
           />
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 2 has been clicked");
             }}
           />
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 3 has been clicked");
             }}
           />
         </div>
         <div>
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 4 has been clicked");
             }}
           />
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 5 has been clicked");
             }}
           />
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 6 has been clicked");
             }}
           />
         </div>
         <div>
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 7 has been clicked");
             }}
           />
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 8 has been clicked");
             }}
           />
-          <button
-            onClick={() => {
+          <Box
+            handleClick={() => {
               console.log("Button 9 has been clicked");
             }}
           />
