1c2209a4d5505958d1f0f9b708e4b218c12178cf
RealAther <Real.Ather@gmail.com>
2019-02-19 02:39:54 +0500

    :new: Add winner logic

--- src/app.js
+++ src/app.js
@@ -35,7 +35,101 @@ class MainApp extends React.Component {
       checked,
       nextTurn
     });
+    this.calculateWinner(checked);
   };
+
+  calculateWinner(checked) {
+    const { size } = this;
+    let winner;
+
+    if (Object.keys(checked).length < size) {
+      return;
+    }
+
+    for (let line = 1; line <= size; line++) {
+      {
+        const firstItem = checked[`r${line}c1`];
+        if (firstItem) {
+          let allItemsAreSame = true;
+          for (let column = 1; column <= size; column++) {
+            if (checked[`r${line}c${column}`] !== firstItem) {
+              allItemsAreSame = false;
+              break;
+            }
+          }
+          if (allItemsAreSame) {
+            winner = firstItem;
+            break;
+          }
+        }
+      }
+      {
+        if (line === 1) {
+          for (let column = 1; column <= size; column++) {
+            const columnValue = checked[`r${line}c${column}`];
+            if (!columnValue) {
+              continue;
+            }
+            let allItemsAreSame = true;
+            for (let columnRow = 1; columnRow <= size; columnRow++) {
+              if (checked[`r${columnRow}c${column}`] !== columnValue) {
+                allItemsAreSame = false;
+                break;
+              }
+            }
+            if (allItemsAreSame) {
+              winner = columnValue;
+              break;
+            }
+          }
+        }
+      }
+      {
+        {
+          if (line === 1) {
+            const firstItemLTR = checked[`r${line}c1`];
+            if (firstItemLTR) {
+              let allItemsAreSame = true;
+              for (let checkingSet = 1; checkingSet <= size; checkingSet++) {
+                if (
+                  checked[`r${checkingSet}c${checkingSet}`] !== firstItemLTR
+                ) {
+                  allItemsAreSame = false;
+                  break;
+                }
+              }
+              if (allItemsAreSame) {
+                winner = firstItemLTR;
+                break;
+              }
+            }
+
+            const firstItemRTL = checked[`r${line}c${size}`];
+            if (firstItemRTL) {
+              let allItemsAreSame = true;
+              for (let checkingSet = 2; checkingSet <= size; checkingSet++) {
+                if (
+                  checked[`r${checkingSet}c${size - checkingSet + 1}`] !==
+                  firstItemRTL
+                ) {
+                  allItemsAreSame = false;
+                  break;
+                }
+              }
+
+              if (allItemsAreSame) {
+                winner = firstItemRTL;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+    if (winner) {
+      this.setState({ winner });
+    }
+  }
 
   renderButton(pathName) {
     const checked = this.state.checked[pathName];
